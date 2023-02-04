729952fcc921c276e40f3f438f53cad0cda9a684
Fauziditya <fauziaditya19082005@gmail.com>
2023-02-04 12:16:12 +0700

    edit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller {
      public function index(){
 
-          $this->load->view(templates/header);
-          $this->load->view(auth/index);
-          $this->load->view(templates/footer);
+          $this->load->view('templates/header');
+          $this->load->view('auth/index');
+          $this->load->view('templates/footer');
 
         
      }
