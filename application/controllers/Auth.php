1b37d834dcd965b80eedc38fe2bcc5d5bb4bd6d3
restuprayudha797 <yudariau1@gmail.com>
2023-02-04 12:14:32 +0700

    controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
      public function index(){
+
+          $this->load->view(templates/header);
+          $this->load->view(auth/index);
+          $this->load->view(templates/footer);
+
         
      }
 
