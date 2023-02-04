5d780386e1169e9104b52ad7da23c37880c9f2c5
restuprayudha797 <yudariau1@gmail.com>
2023-02-04 13:01:55 +0700

    added database

---
+++ application/controllers/Admin.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+     public function index(){
+
+          $this->load->view('templates/header');
+          $this->load->view('admin/index');
+          $this->load->view('templates/footer');
+
+        
+     }
+    
+
+}
